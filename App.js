c18f8f6b125726a0e10f9b63ffa6009b86311917
ongboonhan <edvinongbh@gmail.com>
2022-11-11 08:13:21 +0800

    update v1

--- App.js
+++ App.js
@@ -1,11 +1,45 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import React, { useEffect, useState } from "react";
+import {
+  ActivityIndicator,
+  StyleSheet,
+  Text,
+  TouchableOpacity,
+  View,
+} from "react-native";
 
 export default function App() {
+  const [loading, setLoading] = useState(true);
+  const [arrival, setArrival] = useState("");
+  const BUSSTOP_URL = "https://arrivelah2.busrouter.sg/?id=83139";
+
+  function loadBusStopData() {
+    setLoading(true);
+    fetch(BUSSTOP_URL)
+      .then((response) => response.json())
+      .then((json) => {
+        const myBus = json.services.filter((bus) => bus.no == 155)[0];
+        console.log(myBus.next.time);
+        setArrival(myBus.next.time);
+        setLoading(false);
+      });
+  }
+
+  useEffect(() => {
+    loadBusStopData();
+    const interval = setInterval(loadBusStopData, 5000);
+    return () => clearInterval(interval);
+  }, []);
+
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <Text style={styles.title}>Bus arrival time:</Text>
+      <Text style={styles.arrivalTime}>
+        {loading ? <ActivityIndicator color={"blue"} /> : arrival}
+      </Text>
+
+      <TouchableOpacity style={styles.button} onPress={() => setLoading(false)}>
+        <Text style={styles.buttonText}>Refresh</Text>
+      </TouchableOpacity>
     </View>
   );
 }
@@ -13,8 +47,21 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: "#fff",
+    alignItems: "center",
+    justifyContent: "center",
   },
+  title: {
+    fontSize: 50,
+    marginVertical: 20,
+  },
+  arrivalTime: {
+    fontSize: 40,
+  },
+  button: {
+    backgroundColor: "lightblue",
+    padding: 20,
+    marginVertical: 20,
+  },
+  buttonText: { fontSize: 20 },
 });
